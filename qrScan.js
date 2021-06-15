ed1215be0632a694fffb72f9b4de4681e3027973
T-Felm-Lee <62108434+T-Felm-Lee@users.noreply.github.com>
2021-06-15 18:45:02 +0200

    yeah????

--- qrScan.js
+++ qrScan.js
@@ -1,4 +1,4 @@
-const qrcode = window.qrcode;
+const qrcoderead = window.qrcode;
 
 const video = document.createElement("video");
 const canvasElement = document.getElementById("qr-canvas");
@@ -6,55 +6,53 @@ const canvas = canvasElement.getContext("2d");
 
 const qrResult = document.getElementById("qr-result");
 const outputData = document.getElementById("outputData");
-const btnScanQR = document.getElementById("scan-btn");
+const btnScanQR = document.getElementById("btn-scan-qr");
 
 let scanning = false;
 
-qrcode.callback = (res) => {
-    if (res) {
-        outputData.innerText = res;
-        scanning = false;
+qrcoderead.callback = res => {
+  if (res) {
+    outputData.innerText = res;
+    scanning = false;
 
-        video.srcObject.getTracks().forEach(track => {
-            track.stop();
-        });
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
 
-        qrResult.hidden = false;
-        btnScanQR.hidden = false;
-        canvasElement.hidden = false;
-
-    }
-
+    qrResult.hidden = false;
+    canvasElement.hidden = true;
+    btnScanQR.hidden = false;
+  }
 };
 
 btnScanQR.onclick = () => {
-    navigator.mediaDevices
-        .getUserMedia({video: { facingMode: "environment" } })
-        .then(function(stream) {
-            scanning = true;
-            qrResult.hidden = true;
-            btnScanQR.hidden = true;
-            canvasElement.hidden = false;
-            video.setAttribute("playsinline", true);
-            video.srcObject = stream;
-            video.play();
-            tick();
-            scan();
-        });
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
 };
 
 function tick() {
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-  
-    scanning && requestAnimationFrame(tick);
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
   }
-  
-  function scan() {
-    try {
-      qrcode.decode();
-    } catch (e) {
-      setTimeout(scan, 300);
-    }
-  }
+}
